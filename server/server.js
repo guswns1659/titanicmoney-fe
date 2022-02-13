d10cf6d09da540e205c8bb717d05a1a84bf17f94
guswns1659 <guswns1659@gmail.com>
2022-02-14 06:25:46 +0900

    feat: Set middleware to app

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+
 const port = process.env.SERVER_PORT;
+
+// Remove cache
+app.disable('etag');
+
+// middleware setting
+app.use(express.static(path.join(__dirname, '../build'))); // TODO(jack.comeback) : ?
+app.use(cookieParser());
+app.use(express.json());
+
