7dbbc98df494a52c6527ecd60c84ef2fcc028b3a
guswns1659 <guswns1659@gmail.com>
2022-02-15 06:36:48 +0900

    TM-10 refactor: refactor components

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,9 +1,13 @@
 import "../css/Balance.css"
+import {useState} from "react";
 
 const Balance = () => {
+
+    const [amount, setAmount] = useState(10000);
+
     return (
         <div className="balance">
-            <div className="balance-amount">10,000원</div>
+            <div className="balance-amount">{amount}원</div>
             <div className="balance-title">타이타닉머니</div>
         </div>
     )
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,11 +1,10 @@
 import '../css/TopNav.css'
 
-
 const TopNav = () => {
     return (
         <div className="topnav">
-            <a className="active" href="#home">타이타닉머니</a>
-            <div className="profile">jack.comeback</div>
+            <a href="/">타이타닉머니</a>
+            <a id="profile" href="/">jack.comeback</a>
         </div>
     )
 }
