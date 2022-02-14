8b44ccbf7614633edc8af44678fbd95bb0998f0f
guswns1659 <guswns1659@gmail.com>
2022-02-14 22:33:07 +0900

    TM-10 feat: Add Balance Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import TestForm from "./components/TestForm";
 import TopNav from "./components/TopNav";
+import SideBar from "./components/SideBar";
+import Balance from "./components/Balance";
 
 function App() {
   return (
       <div className="App">
           <TopNav></TopNav>
+          <SideBar></SideBar>
+          <Balance></Balance>
       </div>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import "../css/Balance.css"
+
+const Balance = () => {
+    return (
+        <div className="balance">
+            <div className="balance-amount">10,000원</div>
+            <div className="balance-title">타이타닉머니</div>
+        </div>
+    )
+}
+
+export default Balance;
