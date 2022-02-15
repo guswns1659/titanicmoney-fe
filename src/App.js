d9b211d3e6bcb255db5cb05a8aef423e7a68c73c
guswns1659 <guswns1659@gmail.com>
2022-02-16 06:58:25 +0900

    test: use css function in styled-components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Circle = styled.div`
   width: 5rem;
   height: 5rem;
-  background: black;
+  background: ${props => props.color || 'black'};
   border-radius: 50%;
-`
+  ${props => 
+    props.huge && 
+    css`
+    width: 10rem;
+    height: 10rem;`}
+`;
 
 function App() {
     return (
-        <Circle></Circle>
+        <Circle color="blue" huge ></Circle>
     );
 }
 
