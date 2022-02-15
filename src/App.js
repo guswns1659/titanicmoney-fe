4e5145370e590fe115e34b3fbcd867fce9d6708a
guswns1659 <guswns1659@gmail.com>
2022-02-16 06:48:44 +0900

    test: Add Circle with styled-components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
-import TopNav from "./components/TopNav";
-import SideBar from "./components/SideBar";
-import Balance from "./components/Balance";
-import Center from "./components/Center";
-import Charge from "./components/Charge";
-import {BrowserRouter, Route} from "react-router-dom";
+import styled from "styled-components";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: black;
+  border-radius: 50%;
+`
 
 function App() {
     return (
-        <div className="App">
-            <TopNav></TopNav>
-            <SideBar></SideBar>
-            <Balance></Balance>
-            <Route exact path = "/" component={Center}/>
-            <Route path = "/charge" component={Charge}/>
-        </div>
+        <Circle></Circle>
     );
 }
 
